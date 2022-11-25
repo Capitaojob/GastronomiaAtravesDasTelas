7a3ff7a78d8cc361822e0a5b2e0900be1d867804
Joao Bataglia <jpangottibataglia@gmail.com>
2022-11-25 12:14:19 -0300

    removed about page, added css and html for ebook page, added css and html for contact page

--- script.js
+++ script.js
@@ -10,3 +10,29 @@ document.querySelector(".fa-xmark").addEventListener("click", () => {
 document.querySelector(".fa-bars").addEventListener("click", () => {
     navLinks.style.right = "0"
 })
+
+leFestin = document.getElementById("leFestin")
+    leFestin.play();
+
+setInterval(changeImages, 3000);
+
+function changeImages( )
+{
+    let nextImage = document.querySelector('.nextImg')
+        nextImage.classList.remove("nextImg")
+        nextImage.classList.remove("smallImg")
+        nextImage.classList.remove("rightImg")
+
+    
+    let smallImage = document.querySelector('.smallImg')
+        smallImage.classList.remove("leftImg")
+        smallImage.classList.add("nextImg")
+        smallImage.classList.add("rightImg")
+    
+    let bigImage = document.querySelector('.bigImg')
+        bigImage.classList.remove('bigImg')
+        bigImage.classList.add("leftImg")
+        bigImage.classList.add('smallImg')
+        nextImage.classList.add('bigImg')
+}
+
